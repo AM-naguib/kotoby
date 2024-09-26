cb3a6aaca79273cdf74d59828b0ba7366afa4b3f
AM-naguib <naguibtread@gmail.com>
2024-09-27 00:57:29 +0300

    add author , section , keyword model

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -10,4 +10,10 @@ class Author extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
-use App\Http\Controllers\Dashboard\SectionController;
+use App\Jobs\testt;
+use App\Jobs\KutubypdfJob;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use App\Http\Controllers\Dashboard\BookController;
 use App\Http\Controllers\Dashboard\AuthorController;
+use App\Http\Controllers\Dashboard\SectionController;
 use App\Http\Controllers\Dashboard\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -45,4 +52,41 @@ Route::prefix("dashboard")->name("dashboard.")->middleware("auth")->group(functi
     Route::resource("authors", AuthorController::class);
 
     Route::resource("sections", SectionController::class);
+
+    Route::resource("books", BookController::class);
+
 });
+
+
+
+
+
+
+
+
+
+Route::get('test', function () {
+    $command = 'start /B php ' . base_path('artisan') . ' sendpages:queue';
+    pclose(popen($command, 'r'));
+
+    return 'Command is running in the background!';
+});
+
+
+
+
+// Route::get('test', function () {
+//     // تشغيل الـ command في الخلفية
+//     exec('php ' . base_path('artisan') . ' sendpages:queue > NUL &');
+
+//     // إرسال استجابة فورية إلى المستخدم
+//     echo 'Command is running in the background!';
+
+//     // إنهاء الاستجابة وإغلاق الاتصال مع العميل
+//     if (function_exists('fastcgi_finish_request')) {
+//         fastcgi_finish_request();
+//     }
+
+//     // استمرار تشغيل الكود في الخلفية بعد إنهاء الاتصال
+// });
+
