683e369d7e403092e8700525bbc4ef7ed9c7ba6b
AM-naguib <naguibtread@gmail.com>
2024-10-02 01:02:41 +0300

    edit in meta

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -3,6 +3,14 @@
 @section('description', getSetting()->description)
 @section('keywords', getSetting()->keywords)
 
+
+@section('meta')
+    <meta name="url" content="{{ route('front.index') }}">
+    <meta name="image" content="https://www.4readlib.com/images/logo.png">
+    <meta property="og:title" content="{{ getSetting()->site_title }}">
+    <meta property="og:description" content="{{ getSetting()->description }}">
+    <meta property="og:url" content="{{ route('front.index') }}">
+@endsection
 @section('schema')
     <script type="application/ld+json">
     {
@@ -108,9 +116,9 @@
                                         viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;"
                                         xml:space="preserve" width="20" height="20">
                                         <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23
-                                                        s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92
-                                                        c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17
-                                                        s-17-7.626-17-17S14.61,6,23.984,6z"></path>
+                                                                s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92
+                                                                c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17
+                                                                s-17-7.626-17-17S14.61,6,23.984,6z"></path>
                                         <g>
                                         </g>
                                         <g>
