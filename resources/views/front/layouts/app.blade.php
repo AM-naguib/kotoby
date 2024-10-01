a43e4a274e67140333da5beeec2df47b007175d6
AM-naguib <naguibtread@gmail.com>
2024-10-02 01:48:44 +0300

    update logo and icon

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -5,16 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') - {{getSetting()->site_title}}</title>
+    <title>@yield('title') - {{ getSetting()->site_title }}</title>
     <meta name="description" content="@yield('description')">
     <meta name="keywords" content="@yield('keywords')">
-    @yield("meta")
+    @yield('meta')
+    <link rel="icon" type="image/x-icon" href="{{env('APP_URL')}}/storage/{{getSetting()->site_favicon}}">
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Main CSS -->
     <link rel="stylesheet" href="{{ asset('front/style.css') }}">
+
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
@@ -27,13 +29,13 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <script src="
-        https://cdn.jsdelivr.net/npm/sweetalert2@11.14.1/dist/sweetalert2.all.min.js
-        "></script>
+                https://cdn.jsdelivr.net/npm/sweetalert2@11.14.1/dist/sweetalert2.all.min.js
+                "></script>
     <link href="
 https://cdn.jsdelivr.net/npm/sweetalert2@11.14.1/dist/sweetalert2.min.css
 " rel="stylesheet">
 
-@yield("schema")
+    @yield('schema')
 </head>
 
 <body>
@@ -43,14 +45,20 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.14.1/dist/sweetalert2.min.css
             <div class="container">
                 <div class="d-flex justify-content-between align-items-center w-100">
                     <div class="left">
-                        <a class="navbar-brand" href="#">
-                            <img src="https://www.kotobati.com/sites/default/files/logo.svg" alt="Logo">
+                        <a class="navbar-brand" href="{{ route('front.index') }}">
+                            @if (getSetting()->site_logo)
+                                <img src="{{ env('APP_URL') . '/storage/' . getSetting()->site_logo }}" alt="Logo">
+                            @else
+                                {{ getSetting()->site_title }}
+                            @endif
                         </a>
                     </div>
                     <div class="login-menu">
                         @guest
-                        <a href="{{ route('login.create') }}" class="btn btn-danger text-white rounded-pill d-none d-md-inline-block">تسجيل الدخول</a>
-                        <a href="{{ route('register.create') }}" class="btn btn-primary text-white rounded-pill d-none d-md-inline-block">انشاء حساب</a>
+                            <a href="{{ route('login.create') }}"
+                                class="btn btn-danger text-white rounded-pill d-none d-md-inline-block">تسجيل الدخول</a>
+                            <a href="{{ route('register.create') }}"
+                                class="btn btn-primary text-white rounded-pill d-none d-md-inline-block">انشاء حساب</a>
 
                         @endguest
 
@@ -97,12 +105,12 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.14.1/dist/sweetalert2.min.css
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 my-3">
                     @guest
 
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('login.create') }}">تسجيل الدخول</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('register.create') }}">انشاء حساب</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ route('login.create') }}">تسجيل الدخول</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ route('register.create') }}">انشاء حساب</a>
+                        </li>
                     @endguest
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="#">حقوق الملكية</a>
@@ -121,14 +129,14 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.14.1/dist/sweetalert2.min.css
                 <div class="row">
                     <div class="col-3">
                         <a href="{{ route('front.index') }}">
-                        <div class="item">
-                            <svg viewBox="0 1 511 511.999" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="m498.699219 222.695312c-.015625-.011718-.027344-.027343-.039063-.039062l-208.855468-208.847656c-8.902344-8.90625-20.738282-13.808594-33.328126-13.808594-12.589843 0-24.425781 4.902344-33.332031 13.808594l-208.746093 208.742187c-.070313.070313-.144532.144531-.210938.214844-18.28125 18.386719-18.25 48.21875.089844 66.558594 8.378906 8.382812 19.441406 13.234375 31.273437 13.746093.484375.046876.96875.070313 1.457031.070313h8.320313v153.695313c0 30.417968 24.75 55.164062 55.167969 55.164062h81.710937c8.285157 0 15-6.71875 15-15v-120.5c0-13.878906 11.292969-25.167969 25.171875-25.167969h48.195313c13.878906 0 25.167969 11.289063 25.167969 25.167969v120.5c0 8.28125 6.714843 15 15 15h81.710937c30.421875 0 55.167969-24.746094 55.167969-55.164062v-153.695313h7.71875c12.585937 0 24.421875-4.902344 33.332031-13.8125 18.359375-18.367187 18.367187-48.253906.027344-66.632813zm-21.242188 45.421876c-3.238281 3.238281-7.542969 5.023437-12.117187 5.023437h-22.71875c-8.285156 0-15 6.714844-15 15v168.695313c0 13.875-11.289063 25.164062-25.167969 25.164062h-66.710937v-105.5c0-30.417969-24.746094-55.167969-55.167969-55.167969h-48.195313c-30.421875 0-55.171875 24.75-55.171875 55.167969v105.5h-66.710937c-13.875 0-25.167969-11.289062-25.167969-25.164062v-168.695313c0-8.285156-6.714844-15-15-15h-22.328125c-.234375-.015625-.464844-.027344-.703125-.03125-4.46875-.078125-8.660156-1.851563-11.800781-4.996094-6.679688-6.679687-6.679688-17.550781 0-24.234375.003906 0 .003906-.003906.007812-.007812l.011719-.011719 208.847656-208.839844c3.234375-3.238281 7.535157-5.019531 12.113281-5.019531 4.574219 0 8.875 1.78125 12.113282 5.019531l208.800781 208.796875c.03125.03125.066406.0625.097656.09375 6.644531 6.691406 6.632813 17.539063-.03125 24.207032zm0 0">
-                                </path>
-                            </svg> <span>الرئيسية</span>
-                        </div>
-                    </a>
+                            <div class="item">
+                                <svg viewBox="0 1 511 511.999" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="m498.699219 222.695312c-.015625-.011718-.027344-.027343-.039063-.039062l-208.855468-208.847656c-8.902344-8.90625-20.738282-13.808594-33.328126-13.808594-12.589843 0-24.425781 4.902344-33.332031 13.808594l-208.746093 208.742187c-.070313.070313-.144532.144531-.210938.214844-18.28125 18.386719-18.25 48.21875.089844 66.558594 8.378906 8.382812 19.441406 13.234375 31.273437 13.746093.484375.046876.96875.070313 1.457031.070313h8.320313v153.695313c0 30.417968 24.75 55.164062 55.167969 55.164062h81.710937c8.285157 0 15-6.71875 15-15v-120.5c0-13.878906 11.292969-25.167969 25.171875-25.167969h48.195313c13.878906 0 25.167969 11.289063 25.167969 25.167969v120.5c0 8.28125 6.714843 15 15 15h81.710937c30.421875 0 55.167969-24.746094 55.167969-55.164062v-153.695313h7.71875c12.585937 0 24.421875-4.902344 33.332031-13.8125 18.359375-18.367187 18.367187-48.253906.027344-66.632813zm-21.242188 45.421876c-3.238281 3.238281-7.542969 5.023437-12.117187 5.023437h-22.71875c-8.285156 0-15 6.714844-15 15v168.695313c0 13.875-11.289063 25.164062-25.167969 25.164062h-66.710937v-105.5c0-30.417969-24.746094-55.167969-55.167969-55.167969h-48.195313c-30.421875 0-55.171875 24.75-55.171875 55.167969v105.5h-66.710937c-13.875 0-25.167969-11.289062-25.167969-25.164062v-168.695313c0-8.285156-6.714844-15-15-15h-22.328125c-.234375-.015625-.464844-.027344-.703125-.03125-4.46875-.078125-8.660156-1.851563-11.800781-4.996094-6.679688-6.679687-6.679688-17.550781 0-24.234375.003906 0 .003906-.003906.007812-.007812l.011719-.011719 208.847656-208.839844c3.234375-3.238281 7.535157-5.019531 12.113281-5.019531 4.574219 0 8.875 1.78125 12.113282 5.019531l208.800781 208.796875c.03125.03125.066406.0625.097656.09375 6.644531 6.691406 6.632813 17.539063-.03125 24.207032zm0 0">
+                                    </path>
+                                </svg> <span>الرئيسية</span>
+                            </div>
+                        </a>
                     </div>
                     <div class="col-3">
                         <a href="{{ route('front.best') }}">
