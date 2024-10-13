4c904c6e127c5f09101b7dfa1f649b6c3dde6664
AM-naguib <naguibtread@gmail.com>
2024-10-13 17:59:10 +0300

    update

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -116,9 +116,9 @@
                                         viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;"
                                         xml:space="preserve" width="20" height="20">
                                         <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23
-                                                                s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92
-                                                                c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17
-                                                                s-17-7.626-17-17S14.61,6,23.984,6z"></path>
+                                                                    s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92
+                                                                    c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17
+                                                                    s-17-7.626-17-17S14.61,6,23.984,6z"></path>
                                         <g>
                                         </g>
                                         <g>
@@ -187,10 +187,11 @@
                                 </h3>
                                 <p class="author-label">
                                     @if ($book->author && $book->author->slug)
-                                    <a href="{{ route('front.page', ['type' => 'author', 'slug' => $book->author->slug]) }}">
-                                        {{ $book->author->name }}
-                                    </a>
-                                @endif
+                                        <a
+                                            href="{{ route('front.page', ['type' => 'author', 'slug' => $book->author->slug]) }}">
+                                            {{ $book->author->name }}
+                                        </a>
+                                    @endif
                                     @if (!$book->isFavorite())
                                         <i class="fa-regular fa-bookmark mx-1 text-danger fs-5" style="cursor:pointer"
                                             onclick="addFavorite({{ $book->id }})"></i>
@@ -217,7 +218,7 @@
     <script>
         function addFavorite(bookId) {
             let url = "{{ route('profile.add.favourite', ':id') }}".replace(':id', bookId);
-
+ 
             // التأكد أن الرابط يستخدم HTTPS
             if (!url.startsWith('https://')) {
                 url = url.replace('http://', 'https://');
