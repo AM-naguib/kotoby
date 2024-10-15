3116472be085cdf9d9af1b12b9b8ce951f47a017
AM-naguib <naguibtread@gmail.com>
2024-10-15 18:19:54 +0300

    head update seo

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -2,12 +2,22 @@
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - {{ getSetting()->site_title }}</title>
+    <link rel="canonical" href="https://kotoby.ink/" />
+
     <meta name="description" content="@yield('description')">
     <meta name="keywords" content="@yield('keywords')">
+    <!-- OpenGraph Meta Tags -->
+<meta property="og:title" content="@yield('title') - {{ getSetting()->site_title }}">
+<meta property="og:description" content="@yield('description')">
+<meta property="og:url" content="{{env('APP_URL')}}">
+<meta property="og:image" content="{{env('APP_URL')}}/storage/{{getSetting()->site_favicon}}">
+<meta property="og:type" content="website">
+
     @yield('meta')
     <link rel="icon" type="image/x-icon" href="{{env('APP_URL')}}/storage/{{getSetting()->site_favicon}}">
 
